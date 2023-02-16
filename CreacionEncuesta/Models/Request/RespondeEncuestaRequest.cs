49f40bd89ed0b27bc2925b3d65c521124fc85c94
Anthonygmz <79477732+AnthonyGom9@users.noreply.github.com>
2023-02-16 16:44:46 -0600

    Subida

--- CreacionEncuesta/Models/Request/RespondeEncuestaRequest.cs
+++ CreacionEncuesta/Models/Request/RespondeEncuestaRequest.cs
@@ -5,6 +5,9 @@ namespace CreacionEncuesta.Models.Request
     public class SelectEncuestaRequest
     {
         [Required]
-        public string Name{ get; set; }
+        public string NombreEncuesta { get; set; }
+
+        [Required]
+        public List<RespuestaEncuestum> respuestaencuesta { get; set; }
     }
 }
